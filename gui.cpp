56edcc1de284045ba26b32b14e5147c02d301e7f
Quentin Santos <qsantos@qsantos.fr>
2019-08-05 22:22:31 +0200

    Enable multisampling

--- gui.cpp
+++ gui.cpp
@@ -461,6 +461,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     GLFWwindow* window = glfwCreateWindow(1024, 768, "Orbit", NULL, NULL);
     if (window == NULL) {
