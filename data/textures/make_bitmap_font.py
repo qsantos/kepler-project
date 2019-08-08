e4c4ac182f7aa7176067b0dc82b62e4b32655f18
Quentin Santos <qsantos@qsantos.fr>
2019-08-08 19:55:14 +0200

    Add textures to versioning

---
+++ data/textures/make_bitmap_font.py
@@ -0,0 +1,29 @@
+"""Create bitmap font using ImageMagick tools"""
+
+import subprocess
+
+# characters to render
+charset = ''.join(chr(c) for c in range(32, 128))
+
+# rows of 16 columns
+charset = '\n'.join(''.join(charset[16 * i:16 * (i + 1)]) for i in range(6))
+
+# escape special chars
+charset = charset.replace('%', '%%')
+charset = charset.replace('\\', r'\\')
+charset = charset.replace(' ', r'\ ')
+
+# width of underscore cause image to shift, so add later
+charset = charset.replace('_', ' ')
+
+subprocess.call([
+    'convert',
+    '-pointsize', '16',
+    '-background', 'none',
+    '-fill', 'white',
+    '-font', 'Courier',  # monospace font
+    '-size', '160x114',  # characters are 10x19
+    'label:' + charset,
+    'label:_', '-geometry', '+150+57', '-composite',  # put back underscore
+    'font.png',
+])
