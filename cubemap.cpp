59f29252d6b4eb0af1286cb4eeff98d49f8f1935
Quentin Santos <qsantos@qsantos.fr>
2019-08-08 19:15:49 +0200

    Loading of greyscale and transparent textures

--- cubemap.cpp
+++ cubemap.cpp
@@ -40,7 +40,7 @@ Cubemap::Cubemap(double size, const char* texture_path) :
         }
         glTexImage2D(
             GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-            0, GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+            0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data
         );
     }
 
--- texture.c
+++ texture.c
@@ -1,15 +1,15 @@
+#include "texture.h"
+
 #include "util.h"
 
-#include <GL/gl.h>
-#include <GL/glext.h>
+#include <GL/glew.h>
 #define STBI_FAILURE_USERMSG
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
-
 unsigned char* load_image(const char* filename, int* width, int* height) {
     stbi_set_flip_vertically_on_load(1);
-    unsigned char* data = stbi_load(filename, width, height, NULL, 3);
+    unsigned char* data = stbi_load(filename, width, height, NULL, 4);
     if (data == NULL) {
         fprintf(stderr, "WARNING: failed to load image %s\n", filename);
         return 0;
@@ -30,7 +30,7 @@ unsigned load_texture(const char* filename) {
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
