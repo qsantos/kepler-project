952091ddedf06dc9d0a56235bee32eef3f5b9406
Quentin Santos <qsantos@qsantos.fr>
2019-08-15 09:26:32 +0200

    Do not use STL in cubemap.cpp

--- cubemap.cpp
+++ cubemap.cpp
@@ -1,21 +1,66 @@
 extern "C" {
 #include "texture.h"
+#include "util.h"
 }
 #include "cubemap.hpp"
 
-#include <string>
+#include <cstring>
 
-using std::string;
-
-// TODO: duplicated code
-// how is this not part of the STL?
-static string replace(string s, string pattern, string replacement) {
-    size_t pos = s.find(pattern);
-    while (pos != string::npos) {
-        s.replace(pos, pattern.size(), replacement);
-        pos = s.find(pattern, pos + replacement.size());
+static size_t count(const char* s, const char* pattern) {
+    size_t matches = 0;
+    if (s == NULL || pattern == NULL) {
+        return 0;
     }
-    return s;
+
+    size_t n_pattern = strlen(pattern);
+    while (1) {
+        s = strstr(s, pattern);
+        if (s == NULL) {
+            break;
+        }
+
+        matches += 1;
+        s += n_pattern;;
+    }
+
+    return matches;
+}
+
+static char* replace(const char* s, const char* pattern, const char* replacement) {
+    count(NULL, pattern);
+
+    size_t n_s = strlen(s);
+    size_t n_pattern = strlen(pattern);
+    size_t n_replacement = strlen(replacement);
+
+    size_t n = n_s + count(s, pattern) * (n_replacement - n_pattern) + 1;
+    char* ret = (char*) MALLOC(n);
+
+    char* cur = ret;
+    while (1) {
+        // find pattern
+        const char* match = strstr(s, pattern);
+        if (match == NULL) {
+            break;
+        }
+
+        // copy s until pattern
+        size_t n_prefix = match - s;
+        memcpy(cur, s, n_prefix);
+        s = match;
+        cur += n_prefix;
+
+        // copy replacement
+        memcpy(cur, replacement, n_replacement);
+        s += n_pattern;
+        cur += n_replacement;
+    }
+
+    // copy rest of s
+    memcpy(cur, s, strlen(s));
+    ret[n - 1] = '\0';
+
+    return ret;
 }
 
 Cubemap::Cubemap(double size, const char* texture_path) :
@@ -32,9 +77,10 @@ Cubemap::Cubemap(double size, const char* texture_path) :
     };
 
     for (GLenum i = 0; i < 6; i += 1) {
-        string path = replace(texture_path, "{}", faces[i]);
+        char* path = replace(texture_path, "{}", faces[i]);
         int width, height;
-        unsigned char* data = load_image(path.c_str(), &width, &height);
+        unsigned char* data = load_image(path, &width, &height);
+        free(path);
         if (data == NULL) {
             continue;
         }
